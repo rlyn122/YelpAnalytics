f7bf5fa7b33fbde8d7ea9e291e27ec12cbc26cf6
rlyn122 <125698385+rlyn122@users.noreply.github.com>
2024-03-21 16:48:21 -0400

    Update filter.py

--- filter.py
+++ filter.py
@@ -5,7 +5,14 @@ df = pd.read_json('yelp_dataset/yelp_academic_dataset_business.json',lines=True)
 print(df)
 
 min = 25
-popular_businesses = df[df['review_count']>min]
-print('number of {min} plus businesses ', len(popular_businesses))
 
+popular_businesses = df[df['review_count']>min]
+print(f"number of {min} plus businesses: ", len(popular_businesses))
+
+popular_restaraunts = popular_businesses[popular_businesses['categories'].str.contains('Restaurants',na=False)]
+print(f"number of restaraunts with {min} reviews: ", len(popular_restaraunts))
+
+restaraunts_by_state = popular_restaraunts.groupby("state").size()
+print("restraunts by state: \n")
+print(restaraunts_by_state)
 
