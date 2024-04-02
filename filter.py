27c3904d5fc7e3003ef4f5b6c6d96fbe3b8ed8f4
Ryan Lin <125698385+rlyn122@users.noreply.github.com>
2024-04-02 14:01:02 -0400

    changed filter.py

--- filter.py
+++ filter.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 print('reading dataset...')
-df = pd.read_json('yelp_dataset/yelp_academic_dataset_business.json',lines=True)
-print(df)
+df = pd.read_json('data/yelp_academic_dataset_business.json',lines=True)
+print(df.head(10))
+
 
 min = 25
 
@@ -13,6 +14,19 @@ popular_restaraunts = popular_businesses[popular_businesses['categories'].str.co
 print(f"number of restaraunts with {min} reviews: ", len(popular_restaraunts))
 
 restaraunts_by_state = popular_restaraunts.groupby("state").size()
+
+chinese_restaraunts = popular_businesses[popular_businesses['categories'].str.contains('Chinese',na=False)]
+print("Chinese restaraunts ", len(chinese_restaraunts))
+
+American_restaraunts = popular_businesses[popular_businesses['categories'].str.contains('American',na=False)]
+print("American restaraunts ", len(American_restaraunts))
+
+Japanese_r = popular_businesses[popular_businesses['categories'].str.contains('Japanese',na=False)]
+print("Japanese restaraunts ", len(Japanese_r))
+
+mexican_restaraunts = popular_businesses[popular_businesses['categories'].str.contains('Mexican',na=False)]
+print("mexican restaraunts ", len(mexican_restaraunts))
+
 print("restraunts by state: \n")
 print(restaraunts_by_state)
 
