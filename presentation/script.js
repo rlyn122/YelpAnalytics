1d91c36a0eab912c1f457af34d6f062b5b0f5246
Ryan Lin <ry18@Ryans-MacBook.local>
2024-04-29 19:33:23 -0400

    presentation work

--- presentation/script.js
+++ presentation/script.js
@@ -8,4 +8,38 @@ function showSection(sectionId) {
     // Show the requested section
     const targetSection = document.getElementById(sectionId);
     targetSection.classList.remove('hidden');
+
+    //update progress bar
+    updateProgressBar(sectionId);
+}
+
+function updateProgressBar(sectionId) {
+    const progressBar = document.getElementById('progress-bar');
+    let progressPercentage = 0;
+    switch (sectionId) {
+        case 'introduction':
+            progressPercentage = 0;
+            break;
+        case 'Setup':
+            progressPercentage = 10;
+            break;
+        case 'Dataset':
+            progressPercentage = 20;
+            break;
+        case 'analysis1':
+            progressPercentage = 40;
+            break;
+        case 'analysis2':
+            progressPercentage = 60;
+            break;
+        case 'analysis3':
+            progressPercentage = 80;
+            break;
+        case 'conclusion':
+            progressPercentage = 100;
+            break;
+        default:
+            progressPercentage = 0; // Resets or any other case
+    }
+    progressBar.style.width = progressPercentage + '%';
 }
